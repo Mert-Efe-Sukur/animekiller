6084ee1b4fa537c650b5c051ae47ef1edbfc445a
Mert Efe Şükür <88542815+Mert-Efe-Sukur@users.noreply.github.com>
2021-11-17 00:08:16 +0300

    Added Files
    N I C E

---
+++ animekiller.bat
@@ -0,0 +1,15 @@
+@echo off
+
+start warning192-258.hta
+timeout /t 3 /nobreak > NUL
+start warning1637-123.hta
+timeout /t 3 /nobreak > NUL
+start warning741-734.hta
+
+timeout /t 3 /nobreak > NUL
+
+:again
+ start tumblr_59247694c2aa3eacc120c030e49aca99_b589c635_1280.jpg
+goto again
+
+pause
